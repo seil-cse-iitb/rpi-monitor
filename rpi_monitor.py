97abbfc2fe0e9cd523eff64771debc819deaaab8
Hareesh <hareesh@cse.iitb.ac.in>
2018-07-20 14:48:27 +0530

    Moving to rpi for working

--- rpi_monitor.py
+++ rpi_monitor.py
@@ -2,7 +2,7 @@ import json
 import os
 import re
 
-conf=""
+conf=json.load(open("config.json"))
 msg={}
 
 def get_rpi_details():
@@ -10,14 +10,14 @@ def get_rpi_details():
     msg['up_time']  = os.popen("uptime -p").read()
     msg['up_since'] = os.popen("uptime -s").read()
 
-    result=os.popen("egrep 'Mem|Cache|Swap' /proc/meminfo").read().split('\n')
-    msg['memory_total'] =  result[0].split(' ')[-2]
-    msg['memory_free'] =  result[1].split(' ')[-2]
-    msg['memory_available'] =  result[2].split(' ')[-2]
-    msg['cached'] =  result[3].split(' ')[-2]
-    msg['swap_cached'] =  result[4].split(' ')[-2]
-    msg['swap_total'] =  result[5].split(' ')[-2]
-    msg['swap_free'] =  result[6].split(' ')[-2]
+    result=os.popen("egrep 'Mem|Cache|Swap' /proc/meminfo | awk '{print $2}").read().split('\n')
+    msg['memory_total'] =  result[0]
+    msg['memory_free'] =  result[1]
+    msg['memory_available'] =  result[2]
+    msg['cached'] =  result[3]
+    msg['swap_cached'] =  result[4]
+    msg['swap_total'] =  result[5]
+    msg['swap_free'] =  result[6]
 
 
     result= os.popen("cat /proc/loadavg").read().split(' ')
@@ -25,11 +25,13 @@ def get_rpi_details():
     msg['load 5_min']=result[1]
     msg['load 15_min']=result[2]
 
-    # result=os.popen("df | grep 'root'").read().split(' ')
+    msg['users']=os.popen("uptime | awk '{print $6}'")
 
     print (msg)
 
 def get_process_stat():
+    process_to_monitor=config['process']
+    print process_to_monitor
     call(["ls", "-l"])
     return 0
 
@@ -39,7 +41,9 @@ def get_service_stat():
     return t
 
 
-get_rpi_details()
+get_process_stat()
+#
+# get_rpi_details()
 
 # def main():
 
